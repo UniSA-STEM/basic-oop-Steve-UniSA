5e400a59666654d83d5ee860e4a1c067ed83fceb
Steve Corns <stevecorns@outlook.com.au>
2025-10-12 15:40:25 +1030

    Added string conversion method to the Asset class

--- Asset.py
+++ Asset.py
@@ -11,8 +11,13 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 class Asset:
     def __init__(self):
         """
-        This class represents a asset object.
+        This class represents an asset object.
         """
         self.__name = ""
         self.__description = ""
         self.__encrypted = False
+        self.__valid_assets = ["CryptoToken", "Data Spike", "Removable Drive", "Security Chip", "Hardware Patch"]
+
+    def __str__(self):
+        return_string = f"{self.__name}:{self.__description} {"Encrypted" if self.__encrypted else ""}"
+        return return_string
